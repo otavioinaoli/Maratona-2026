c615f54bccc28d8129c0b76434795556ac8c4419
otavioinaoli <otaviogame25@gmail.com>
2026-08-04 21:25:03 -0300

    Semana 8

---
+++ semana8/containerWithMostWater.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0, r = height.size() - 1;
+
+        int maxi = 0;
+        while(l < r){
+            maxi = max(maxi, min(height[l],height[r]) * (r - l));
+            if(height[l] > height[r]){
+                r--;
+            }else{
+                l++;
+            }
+        }
+
+        return maxi;
+    }
+};
